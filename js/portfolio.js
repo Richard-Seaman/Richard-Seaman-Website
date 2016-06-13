4c452f147e0b96622600a4ee521ebf8e2641153a
Richard Seaman <rdgseaman@gmail.com>
2016-06-13 20:53:25 +0100

    more projects and tweaked pages

---
+++ js/portfolio.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	
+	// Animations for main buttons	
+	$('figure').hover(
+	    function(){
+	        $(this).addClass('figure_active');
+	        $(this).find('div.shakeable').effect( "shake", { direction: "right", times: 2, distance: 4}, 1000 );
+	    },
+	    function(){
+	        $(this).removeClass('figure_active');
+	    }
+	);
+	
+
+});
